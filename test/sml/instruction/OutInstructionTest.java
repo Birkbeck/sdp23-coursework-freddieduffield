f24e31ef5eb2e1d8c5f3dae0f3508a4e877a5e3c
freddieduffield <fwhduffield@gmail.com>
2023-03-03 13:07:40 +0000

    `OutInstructionTest` failing test

---
+++ test/sml/instruction/OutInstructionTest.java
@@ -0,0 +1,34 @@
+package sml.instruction;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import sml.Instruction;
+import sml.Machine;
+import sml.Registers;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static sml.Registers.Register.EBX;
+
+public class OutInstructionTest {
+    private Machine machine;
+    private Registers registers;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void setup() {
+        machine = new Machine(new Registers());
+        registers = machine.getRegisters();
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @Test
+    void validExecution() {
+        registers.set(EBX, 8);
+        Instruction instruction = new OutInstruction(null, EBX);
+        instruction.execute(machine);
+        Assertions.assertEquals("8", outputStreamCaptor.toString().trim());
+    }
+}
